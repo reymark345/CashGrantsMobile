4ddc32e4ac9834d3c5a8f2460fe58ce6d58258b0
reymark345 <reymarkvaldehueza@gmail.com>
2021-10-23 23:45:56 +0800

    feature/cgt-92: Add Login UI Splash in Mobile App

---
+++ app/src/main/java/com/example/cashgrantsmobile/Splash/Activity_Splash_Login.java
@@ -0,0 +1,34 @@
+package com.example.cashgrantsmobile.Splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cashgrantsmobile.MainActivity;
+import com.example.cashgrantsmobile.R;
+import com.example.cashgrantsmobile.Scanner.ScanCashCard;
+
+public class Activity_Splash_Login extends AppCompatActivity {
+
+    Button BtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+        BtnLogin = (Button) findViewById(R.id.btnAccess);
+
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_Splash_Login.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
